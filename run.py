9e704b7cd7aedc5b35837f8c12bfffd52e5184b8
Amrutha P <88905004+Amrutha0401@users.noreply.github.com>
2025-03-13 03:15:58 +0530

    Update run.py

--- run.py
+++ run.py
@@ -6,12 +6,6 @@ import datetime
 import torch.nn as nn
 import torch.optim as optim
 import torch
-import torch.distributed as dist
-from torch.nn.parallel import DistributedDataParallel as DDP
-import torch.multiprocessing as mp
-from torch.utils.data.distributed import DistributedSampler
-import time
-from utils import AutomaticWeightedLoss
 from model import GraphSmile
 from sklearn.metrics import confusion_matrix, classification_report
 from trainer import train_or_eval_model, seed_everything
@@ -21,7 +15,7 @@ from dataloader import (
     MELDDataset_BERT,
     CMUMOSEIDataset7,
 )
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SubsetRandomSampler
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -117,10 +111,6 @@ parser.add_argument('--Data_path', default='./data', help='data directory to tra
 
 args = parser.parse_args()
 
-os.environ["MASTER_ADDR"] = "localhost"
-world_size = torch.cuda.device_count()
-os.environ["WORLD_SIZE"] = str(world_size)
-
 MELD_path = args.Data_path 
 IEMOCAP_path = ""
 IEMOCAP4_path = ""
@@ -129,63 +119,27 @@ CMUMOSEI7_path = ""
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def get_data_loaders(path, dataset_class, batch_size, valid_ratio, num_workers, pin_memory):
+    full_dataset = dataset_class(path)
+    dataset_size = len(full_dataset)
+    indices = list(range(dataset_size))
+    split = int(valid_ratio * dataset_size)
+    np.random.shuffle(indices)
+    train_indices, valid_indices = indices[split:], indices[:split]
 
-def init_ddp(local_rank):
-    try:
-        if not dist.is_initialized():
-            torch.cuda.set_device(local_rank)
-            os.environ["RANK"] = str(local_rank)
-            dist.init_process_group(backend="nccl", init_method="env://")
-        else:
-            logger.info("Distributed process group already initialized.")
-    except Exception as e:
-        logger.error(f"Failed to initialize distributed process group: {e}")
-        raise
-
-
-def reduce_tensor(tensor: torch.Tensor):
-    rt = tensor.clone()
-    dist.all_reduce(rt, op=dist.ReduceOp.SUM)
-    rt /= dist.get_world_size()
-
-    return rt
-
-
-def get_ddp_generator(seed=3407):
-    local_rank = dist.get_rank()
-    g = torch.Generator()
-    g.manual_seed(seed + local_rank)
-
-    return g
-
-
-def get_train_valid_sampler(trainset, valid_ratio):
-    size = len(trainset)
-    idx = list(range(size))
-    split = int(valid_ratio * size)
-
-    return DistributedSampler(idx[split:]), DistributedSampler(idx[:split])
-
-
-def get_data_loaders(path, dataset_class, batch_size, valid_ratio, num_workers,
-                     pin_memory):
-
-    trainset = dataset_class(path)
-    train_sampler, valid_sampler = get_train_valid_sampler(
-        trainset, valid_ratio)
     train_loader = DataLoader(
-        trainset,
+        full_dataset,
         batch_size=batch_size,
-        sampler=train_sampler,
-        collate_fn=trainset.collate_fn,
+        sampler=SubsetRandomSampler(train_indices),
+        collate_fn=full_dataset.collate_fn,
         num_workers=num_workers,
         pin_memory=pin_memory,
     )
     valid_loader = DataLoader(
-        trainset,
+        full_dataset,
         batch_size=batch_size,
-        sampler=valid_sampler,
-        collate_fn=trainset.collate_fn,
+        sampler=SubsetRandomSampler(valid_indices),
+        collate_fn=full_dataset.collate_fn,
         num_workers=num_workers,
         pin_memory=pin_memory,
     )
@@ -197,28 +151,16 @@ def get_data_loaders(path, dataset_class, batch_size, valid_ratio, num_workers,
         num_workers=num_workers,
         pin_memory=pin_memory,
     )
-
     return train_loader, valid_loader, test_loader
 
-
-def setup_samplers(trainset, valid_ratio, epoch):
-    train_sampler, valid_sampler = get_train_valid_sampler(
-        trainset, valid_ratio=valid_ratio)
-    train_sampler.set_epoch(epoch)
-    valid_sampler.set_epoch(epoch)
-
-
-def main(local_rank):
-    print(f"Running main(**args) on rank {local_rank}.")
-    init_ddp(local_rank)  # 初始化
-
+def main():
     today = datetime.datetime.now()
     name_ = args.modals + "_" + args.dataset
 
     cuda = torch.cuda.is_available() and not args.no_cuda
+    device = torch.device("cuda" if cuda else "cpu")
     if args.tensorboard:
         from tensorboardX import SummaryWriter
-
         writer = SummaryWriter()
 
     n_epochs = args.epochs
@@ -242,15 +184,7 @@ def main(local_rank):
         n_classes_emo = 4
 
     seed_everything()
-    model = GraphSmile(args, embedding_dims, n_classes_emo)
-
-    model = model.to(local_rank)
-    model = DDP(
-        model,
-        device_ids=[local_rank],
-        output_device=local_rank,
-        find_unused_parameters=True,
-    )
+    model = GraphSmile(args, embedding_dims, n_classes_emo).to(device)
 
     loss_function_emo = nn.NLLLoss()
     loss_function_sen = nn.NLLLoss()
@@ -260,13 +194,8 @@ def main(local_rank):
         awl = AutomaticWeightedLoss(3)
         optimizer = optim.AdamW(
             [
-                {
-                    "params": model.parameters()
-                },
-                {
-                    "params": awl.parameters(),
-                    "weight_decay": 0
-                },
+                {"params": model.parameters()},
+                {"params": awl.parameters(), "weight_decay": 0},
             ],
             lr=args.lr,
             weight_decay=args.l2,
@@ -326,17 +255,6 @@ def main(local_rank):
     all_f1_sft, all_acc_sft = [], []
 
     for epoch in range(n_epochs):
-        if args.dataset == "MELD":
-            trainset = MELDDataset_BERT(MELD_path)
-        elif args.dataset == "IEMOCAP":
-            trainset = IEMOCAPDataset_BERT(IEMOCAP_path)
-        elif args.dataset == "IEMOCAP4":
-            trainset = IEMOCAPDataset_BERT4(IEMOCAP4_path)
-        elif args.dataset == "CMUMOSEI7":
-            trainset = CMUMOSEIDataset7(CMUMOSEI7_path)
-
-        setup_samplers(trainset, valid_ratio=0.1, epoch=epoch)
-
         start_time = time.time()
 
         train_loss, _, _, train_acc_emo, train_f1_emo, _, _, train_acc_sen, train_f1_sen, train_acc_sft, train_f1_sft, _, _, _ = train_or_eval_model(
@@ -356,6 +274,7 @@ def main(local_rank):
             args.epochs,
             args.classify,
             args.shift_win,
+            device=device
         )
 
         valid_loss, _, _, valid_acc_emo, valid_f1_emo, _, _, valid_acc_sen, valid_f1_sen, valid_acc_sft, valid_f1_sft, _, _, _ = train_or_eval_model(
@@ -375,6 +294,7 @@ def main(local_rank):
             args.epochs,
             args.classify,
             args.shift_win,
+            device=device
         )
 
         print(
@@ -389,78 +309,76 @@ def main(local_rank):
                 valid_f1_emo,
             ))
 
-        if local_rank == 0:
-            test_loss, test_label_emo, test_pred_emo, test_acc_emo, test_f1_emo, test_label_sen, test_pred_sen, test_acc_sen, test_f1_sen, test_acc_sft, test_f1_sft, _, test_initial_feats, test_extracted_feats = train_or_eval_model(
-                model,
-                loss_function_emo,
-                loss_function_sen,
-                loss_function_shift,
-                test_loader,
-                epoch,
-                cuda,
-                args.modals,
-                None,
-                False,
-                args.dataset,
-                args.loss_type,
-                args.lambd,
-                args.epochs,
-                args.classify,
-                args.shift_win,
-            )
+        test_loss, test_label_emo, test_pred_emo, test_acc_emo, test_f1_emo, test_label_sen, test_pred_sen, test_acc_sen, test_f1_sen, test_acc_sft, test_f1_sft, _, test_initial_feats, test_extracted_feats = train_or_eval_model(
+            model,
+            loss_function_emo,
+            loss_function_sen,
+            loss_function_shift,
+            test_loader,
+            epoch,
+            cuda,
+            args.modals,
+            None,
+            False,
+            args.dataset,
+            args.loss_type,
+            args.lambd,
+            args.epochs,
+            args.classify,
+            args.shift_win,
+            device=device
+        )
 
-            all_f1_emo.append(test_f1_emo)
-            all_acc_emo.append(test_acc_emo)
-            all_f1_sft.append(test_f1_sft)
-            all_acc_sft.append(test_acc_sft)
+        all_f1_emo.append(test_f1_emo)
+        all_acc_emo.append(test_acc_emo)
+        all_f1_sft.append(test_f1_sft)
+        all_acc_sft.append(test_acc_sft)
 
+        print(
+            "test_loss: {}, test_acc_emo: {}, test_f1_emo: {}, test_acc_sen: {}, test_f1_sen: {}, test_acc_sft: {}, test_f1_sft: {}, total time: {} sec, {}"
+            .format(
+                test_loss,
+                test_acc_emo,
+                test_f1_emo,
+                test_acc_sen,
+                test_f1_sen,
+                test_acc_sft,
+                test_f1_sft,
+                round(time.time() - start_time, 2),
+                time.strftime("%Y-%m-%d %H:%M:%S",
+                              time.localtime(time.time())),
+            ))
+        print("-" * 100)
+
+        if args.classify == "emotion":
+            if best_f1_emo is None or best_f1_emo < test_f1_emo:
+                best_f1_emo = test_f1_emo
+                best_f1_sen = test_f1_sen
+                best_label_emo, best_pred_emo = test_label_emo, test_pred_emo
+                best_label_sen, best_pred_sen = test_label_sen, test_pred_sen
+
+        elif args.classify == "sentiment":
+            if best_f1_sen is None or best_f1_sen < test_f1_sen:
+                best_f1_emo = test_f1_emo
+                best_f1_sen = test_f1_sen
+                best_label_emo, best_pred_emo = test_label_emo, test_pred_emo
+                best_label_sen, best_pred_sen = test_label_sen, test_pred_sen
+
+        if (epoch + 1) % 10 == 0:
+            np.set_printoptions(suppress=True)
             print(
-                "test_loss: {}, test_acc_emo: {}, test_f1_emo: {}, test_acc_sen: {}, test_f1_sen: {}, test_acc_sft: {}, test_f1_sft: {}, total time: {} sec, {}"
-                .format(
-                    test_loss,
-                    test_acc_emo,
-                    test_f1_emo,
-                    test_acc_sen,
-                    test_f1_sen,
-                    test_acc_sft,
-                    test_f1_sft,
-                    round(time.time() - start_time, 2),
-                    time.strftime("%Y-%m-%d %H:%M:%S",
-                                  time.localtime(time.time())),
-                ))
+                classification_report(best_label_emo,
+                                      best_pred_emo,
+                                      digits=4,
+                                      zero_division=0))
+            print(confusion_matrix(best_label_emo, best_pred_emo))
+            print(
+                classification_report(best_label_sen,
+                                      best_pred_sen,
+                                      digits=4,
+                                      zero_division=0))
+            print(confusion_matrix(best_label_sen, best_pred_sen))
             print("-" * 100)
-
-            if args.classify == "emotion":
-                if best_f1_emo == None or best_f1_emo < test_f1_emo:
-                    best_f1_emo = test_f1_emo
-                    best_f1_sen = test_f1_sen
-                    best_label_emo, best_pred_emo = test_label_emo, test_pred_emo
-                    best_label_sen, best_pred_sen = test_label_sen, test_pred_sen
-
-            elif args.classify == "sentiment":
-                if best_f1_sen == None or best_f1_sen < test_f1_sen:
-                    best_f1_emo = test_f1_emo
-                    best_f1_sen = test_f1_sen
-                    best_label_emo, best_pred_emo = test_label_emo, test_pred_emo
-                    best_label_sen, best_pred_sen = test_label_sen, test_pred_sen
-
-            if (epoch + 1) % 10 == 0:
-                np.set_printoptions(suppress=True)
-                print(
-                    classification_report(best_label_emo,
-                                          best_pred_emo,
-                                          digits=4,
-                                          zero_division=0))
-                print(confusion_matrix(best_label_emo, best_pred_emo))
-                print(
-                    classification_report(best_label_sen,
-                                          best_pred_sen,
-                                          digits=4,
-                                          zero_division=0))
-                print(confusion_matrix(best_label_sen, best_pred_sen))
-                print("-" * 100)
-
-        dist.barrier()
 
         if args.tensorboard:
             writer.add_scalar("test: accuracy", test_acc_emo, epoch)
@@ -479,62 +397,57 @@ def main(local_rank):
 
     if args.tensorboard:
         writer.close()
-    if local_rank == 0:
-        print("Test performance..")
-        print("Acc: {}, F-Score: {}".format(max(all_acc_emo), max(all_f1_emo)))
-        if not os.path.exists("results/record_{}_{}_{}.pk".format(
-                today.year, today.month, today.day)):
-            with open(
-                    "results/record_{}_{}_{}.pk".format(
-                        today.year, today.month, today.day),
-                    "wb",
-            ) as f:
-                pk.dump({}, f)
+
+    print("Test performance..")
+    print("Acc: {}, F-Score: {}".format(max(all_acc_emo), max(all_f1_emo)))
+    if not os.path.exists("results/record_{}_{}_{}.pk".format(
+            today.year, today.month, today.day)):
         with open(
-                "results/record_{}_{}_{}.pk".format(today.year, today.month,
-                                                    today.day),
-                "rb",
-        ) as f:
-            record = pk.load(f)
-        key_ = name_
-        if record.get(key_, False):
-            record[key_].append(max(all_f1_emo))
-        else:
-            record[key_] = [max(all_f1_emo)]
-        if record.get(key_ + "record", False):
-            record[key_ + "record"].append(
-                classification_report(best_label_emo,
-                                      best_pred_emo,
-                                      digits=4,
-                                      zero_division=0))
-        else:
-            record[key_ + "record"] = [
-                classification_report(best_label_emo,
-                                      best_pred_emo,
-                                      digits=4,
-                                      zero_division=0)
-            ]
-        with open(
-                "results/record_{}_{}_{}.pk".format(today.year, today.month,
-                                                    today.day),
+                "results/record_{}_{}_{}.pk".format(
+                    today.year, today.month, today.day),
                 "wb",
         ) as f:
-            pk.dump(record, f)
-
-        print(
+            pk.dump({}, f)
+    with open(
+            "results/record_{}_{}_{}.pk".format(today.year, today.month,
+                                                today.day),
+            "rb",
+    ) as f:
+        record = pk.load(f)
+    key_ = name_
+    if record.get(key_, False):
+        record[key_].append(max(all_f1_emo))
+    else:
+        record[key_] = [max(all_f1_emo)]
+    if record.get(key_ + "record", False):
+        record[key_ + "record"].append(
             classification_report(best_label_emo,
                                   best_pred_emo,
                                   digits=4,
                                   zero_division=0))
-        print(confusion_matrix(best_label_emo, best_pred_emo))
+    else:
+        record[key_ + "record"] = [
+            classification_report(best_label_emo,
+                                  best_pred_emo,
+                                  digits=4,
+                                  zero_division=0)
+        ]
+    with open(
+            "results/record_{}_{}_{}.pk".format(today.year, today.month,
+                                                today.day),
+            "wb",
+    ) as f:
+        pk.dump(record, f)
 
-    dist.destroy_process_group()
-
+    print(
+        classification_report(best_label_emo,
+                              best_pred_emo,
+                              digits=4,
+                              zero_division=0))
+    print(confusion_matrix(best_label_emo, best_pred_emo))
 
 if __name__ == "__main__":
     print(args)
     print("torch.cuda.is_available():", torch.cuda.is_available())
     print("not args.no_cuda:", not args.no_cuda)
-    n_gpus = torch.cuda.device_count()
-    print(f"Use {n_gpus} GPUs")
-    mp.spawn(fn=main, args=(), nprocs=n_gpus)
+    main()
